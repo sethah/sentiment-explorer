8e914ad36d0a19fc6205dc8417dc3a953cf132b0
sethah <shendrickson@cloudera.com>
2019-08-06 13:16:28 -0700

    ls on start

--- sentiment/start.sh
+++ sentiment/start.sh
@@ -6,4 +6,6 @@ mkdir models
 wget -P ./models/ https://sentiment-explorer.s3-us-west-1.amazonaws.com/nbsvm_imdb_sent_500.pkl
 wget -P ./models/ https://sentiment-explorer.s3-us-west-1.amazonaws.com/bert_base_1000.tar.gz
 
+ls -lh ./models
+
 gunicorn --bind 0.0.0.0:5000 wsgi
