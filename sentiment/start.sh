f3560771ace8d8bdbe4d641cc4f9aa666aeafc12
sethah <shendrickson@cloudera.com>
2019-08-06 12:05:16 -0700

    use wget for model

--- sentiment/start.sh
+++ sentiment/start.sh
@@ -2,6 +2,7 @@
 
 mkdir models
 
-aws s3 sync  s3://sentiment-explorer ./models/
+#aws s3 sync  s3://sentiment-explorer ./models/
+wget -P ./models/ https://sentiment-explorer.s3-us-west-1.amazonaws.com/nbsvm_imdb_sent_500.pkl
 
 gunicorn --bind 0.0.0.0:5000 wsgi
