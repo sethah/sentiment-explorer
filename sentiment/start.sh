80a9204cf4a816bc0e2b471712a1702d53aee162
sethah <seth.hendrickson16@gmail.com>
2019-08-11 21:50:35 -0400

    remove locale and nvidia driver stuff

--- sentiment/start.sh
+++ sentiment/start.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# mkdir models
-
-#aws s3 sync  s3://sentiment-explorer ./models/
-# wget -P /models/ https://sentiment-explorer.s3-us-west-1.amazonaws.com/nbsvm_imdb_sent_500.pkl
-# wget -P /models/ https://sentiment-explorer.s3-us-west-1.amazonaws.com/bert_base_1000.tar.gz
-
 ls -lh /models
 
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_TYPE=en_US.UTF-8
-echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen en_US.UTF-8
-LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 LC_TYPE=en_US.UTF-8 python -c "import sys; print(sys.stdout.encoding)"
-LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 LC_TYPE=en_US.UTF-8 gunicorn --timeout 60 --bind 0.0.0.0:5000 wsgi
+gunicorn --timeout 60 --bind 0.0.0.0:5000 wsgi
